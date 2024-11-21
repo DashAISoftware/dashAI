b25325af23825c7b6e8c5515df97d9aa5c6e2edc
Camila Reyes <careyesla@gmail.com>
2024-11-21 00:06:38 -0300

    feat: Add LLMGenerationTask class for language model generation tasks

---
+++ DashAI/back/tasks/llm_generation_task.py
@@ -0,0 +1,47 @@
+import uuid
+from typing import Any, Optional
+
+from DashAI.back.tasks.base_task import BaseTask
+from DashAI.back.tasks.generative_task import GenerativeTask
+
+
+class LLMGenerationTask(BaseTask):
+    """Base class for image generation tasks.
+
+    Here you can change the methods provided by class Task.
+    """
+
+    metadata: dict = {
+        "inputs_types": [str],
+        "outputs_types": [str],
+        "inputs_cardinality": 1,
+        "outputs_cardinality": 1,
+    }
+
+    def prepare_for_task(self, input: str) -> str:
+        """Change the inputs to suit the image generation task.
+
+        Parameters
+        ----------
+        inputs : str
+            Input to be changed
+
+        Returns
+        -------
+        str
+            Input with the new types
+        """
+        return input
+
+    def process_output(
+        self,
+        output: Any,
+        *args: Any,
+    ) -> str:
+        """Process the output of a generative model.
+
+        file_name (Str): Indicates the name of the file.
+        path (Str): Indicates the path where the output will be stored.
+        """
+
+        return output
