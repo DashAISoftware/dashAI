1b58df4af701eebeab90a6da2bff98f90ca2c6e9
Irozuku <emiliodu@gmail.com>
2025-05-12 13:14:50 -0400

    Fix context window size retrieval from gemma model

--- DashAI/back/models/hugging_face/gemma_model.py
+++ DashAI/back/models/hugging_face/gemma_model.py
@@ -1,14 +1,15 @@
 from typing import List
+
 from llama_cpp import Llama
 
 from DashAI.back.core.schema_fields import (
     BaseSchema,
-    int_field,
     float_field,
+    int_field,
     schema_field,
 )
-
 from DashAI.back.models.llm_generation_model import LLMGenerationModel
+
 
 class GemmaSchema(BaseSchema):
     """Schema for Gemma model."""
@@ -17,7 +18,7 @@ class GemmaSchema(BaseSchema):
         int_field(ge=1),
         placeholder=100,
         description="Maximum number of tokens to generate.",
-    )  # type: ignore   
+    )  # type: ignore
 
     temperature: schema_field(
         float_field(ge=0.0, le=1.0),
@@ -35,7 +36,8 @@ class GemmaSchema(BaseSchema):
         int_field(ge=1),
         placeholder=512,
         description="Maximum number of tokens the model can process in a single forward pass (context window size).",
-    ) # type: ignore
+    )  # type: ignore
+
 
 class GemmaModel(LLMGenerationModel):
     """Gemma model for text generation using llama.cpp library."""
@@ -48,23 +50,31 @@ class GemmaModel(LLMGenerationModel):
         self.temperature = kwargs.pop("temperature", 0.7)
         self.frequency_penalty = kwargs.pop("frequency_penalty", 0.1)
         self.n_ctx = kwargs.pop("n_ctx", 512)
-        
+
         self.model_id = "ggml-org/gemma-1.1-7b-it-Q4_K_M-GGUF"
         self.filename = "*.gguf"
 
         self.model = Llama.from_pretrained(
-            repo_id=self.model_id, filename=self.filename, verbose=True, n_ctx=self.n_ctx,
+            repo_id=self.model_id,
+            filename=self.filename,
+            verbose=True,
+            n_ctx=self.n_ctx,
         )
 
     # def generate(self, prompt: str, history: list[tuple[str, str]]) -> str:
     def generate(self, prompt: str) -> List[str]:
         """Generate text based on prompts."""
         full_prompt = f"Q: {prompt} A:"
-        if len(full_prompt) > self.model.n_ctx:
-            full_prompt = full_prompt[-self.model.n_ctx:]
+        if len(full_prompt) > self.model.n_ctx():
+            full_prompt = full_prompt[-self.model.n_ctx() :]
 
         output = self.model(
-            full_prompt, max_tokens=self.max_tokens, temperature=self.temperature, frequency_penalty=self.frequency_penalty, stop=["Q:"], echo=False
+            full_prompt,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            stop=["Q:"],
+            echo=False,
         )
         generated_text = output["choices"][0]["text"]
         clean_text = generated_text.replace(f"Q: {prompt} A:", "").strip()
