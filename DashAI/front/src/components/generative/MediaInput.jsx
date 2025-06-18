1483045350e388438ff0e5b50f44df4bda7f42e0
Irozuku <emiliodu@gmail.com>
2025-06-18 02:43:06 -0400

    Remove hidden "Add image" label from MediaInput component

--- DashAI/front/src/components/generative/MediaInput.jsx
+++ DashAI/front/src/components/generative/MediaInput.jsx
@@ -152,21 +152,6 @@ export function MediaInput({
             }}
           >
             <ImageIcon sx={{ fontSize: 20 }} />
-            <Box
-              component="span"
-              sx={{
-                position: "absolute",
-                width: 1,
-                height: 1,
-                padding: 0,
-                overflow: "hidden",
-                clip: "rect(0, 0, 0, 0)",
-                whiteSpace: "nowrap",
-                border: 0,
-              }}
-            >
-              Add image
-            </Box>
           </Box>
           <Box
             component="input"
