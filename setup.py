f3cc5c70125d6c218e09dea3b0c0a3b4e15b4511
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2025-06-19 14:59:51 -0400

    Added workflow to push to pypi in production

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = load_requirements("requirements-dev.txt")
 
 setup(
     name="DashAI",
-    version="0.1.5",
+    version="0.1.6",
     license="MIT",
     description=(
         "DashAI: a graphical toolbox for training, evaluating and deploying "
