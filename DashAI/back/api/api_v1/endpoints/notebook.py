1ef1cd44c6c938deac468055e9b8b24ed33ebca1
Irozuku <emiliodu@gmail.com>
2025-08-05 17:56:13 -0400

    feat: add endpoints to retrieve all explorers and converters associated with a notebook

--- DashAI/back/api/api_v1/endpoints/notebook.py
+++ DashAI/back/api/api_v1/endpoints/notebook.py
@@ -5,8 +5,9 @@ from fastapi.exceptions import HTTPException
 from kink import di, inject
 from sqlalchemy.orm import Session, sessionmaker
 
+from DashAI.back.api.api_v1.schemas import explorers_params as explorer_schemas
 from DashAI.back.api.api_v1.schemas import notebook_params as schemas
-from DashAI.back.dependencies.database.models import Notebook
+from DashAI.back.dependencies.database.models import ConverterList, Explorer, Notebook
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -20,6 +21,26 @@ def create_notebook(
     params: schemas.NotebookCreate,
     session_factory: sessionmaker = Depends(lambda: di["session_factory"]),
 ):
+    """Create a new notebook entry in the database.
+
+    Parameters
+    ----------
+    params : schemas.NotebookCreate
+        The parameters for creating a notebook.
+    session_factory : Callable[..., ContextManager[Session]]
+        A factory that creates a context manager that handles a SQLAlchemy session.
+        The generated session can be used to access and query the database.
+
+    Returns
+    -------
+    schemas.Notebook
+        The newly created notebook object.
+
+    Raises
+    ------
+    HTTPException
+        If there is an error creating the notebook, returns a 500 Internal Server Error.
+    """
     db: Session
     with session_factory() as db:
         try:
@@ -44,6 +65,26 @@ def get_notebook(
     notebook_id: int,
     session_factory: sessionmaker = Depends(lambda: di["session_factory"]),
 ):
+    """Get a notebook by its ID.
+
+    Parameters
+    ----------
+    notebook_id : int
+        ID of the notebook to retrieve.
+    session_factory : Callable[..., ContextManager[Session]]
+        A factory that creates a context manager that handles a SQLAlchemy session.
+        The generated session can be used to access and query the database.
+
+    Returns
+    -------
+    schemas.Notebook
+        The notebook object with the specified ID.
+
+    Raises
+    ------
+    HTTPException
+        If the notebook is not found, returns a 404 Not Found error.
+    """
     db: Session
     with session_factory() as db:
         notebook = db.query(Notebook).filter(Notebook.id == notebook_id).first()
@@ -53,3 +94,87 @@ def get_notebook(
                 detail="Notebook not found",
             )
         return notebook
+
+
+@router.get("/{notebook_id}/explorer")
+@inject
+def get_notebook_explorer(
+    notebook_id: int,
+    session_factory: sessionmaker = Depends(lambda: di["session_factory"]),
+):
+    """Get all explorers associated with a notebook.
+
+    Parameters
+    ----------
+    notebook_id : int
+        ID of the notebook.
+    session_factory : Callable[..., ContextManager[Session]]
+        A factory that creates a context manager that handles a SQLAlchemy session.
+        The generated session can be used to access and query the database.
+
+    Returns
+    -------
+    explorers : List[explorer_schemas.Explorer]
+        List of explorers associated with the notebook.
+
+    Raises
+    ------
+    HTTPException
+        If there is an error retrieving explorers, returns a 500 Internal Server Error.
+    """
+    db: Session
+    with session_factory() as db:
+        try:
+            explorers = (
+                db.query(Explorer).filter(Explorer.notebook_id == notebook_id).all()
+            )
+            return explorers
+        except Exception as e:
+            log.error(f"Error retrieving explorers for notebook {notebook_id}: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Failed to retrieve explorers",
+            )
+
+
+@router.get("/{notebook_id}/converter")
+@inject
+async def post_notebook_converter_list(
+    notebook_id: int,
+    session_factory: sessionmaker = Depends(lambda: di["session_factory"]),
+):
+    """Get all converters associated with a notebook.
+
+    Parameters
+    ----------
+    notebook_id : int
+        ID of the notebook.
+    session_factory : Callable[..., ContextManager[Session]]
+        Dependency-injected SQLAlchemy session factory.
+
+    Returns
+    -------
+    ConverterList
+        The converter list associated with the notebook.
+
+    Raises
+    ------
+        HTTPException: If there is an error retrieving the converter list, returns a 500 Internal Server Error.
+    """
+    with session_factory() as db:
+        try:
+            converter_list = (
+                db.query(ConverterList)
+                .filter(ConverterList.notebook_id == notebook_id)
+                .all()
+            )
+
+            return converter_list
+        except Exception as e:
+            log.error(
+                f"Error retrieving converter list for notebook {notebook_id}: {e}"
+            )
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Failed to retrieve converter from notebook {notebook_id}",
+            )
