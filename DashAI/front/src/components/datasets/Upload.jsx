25801d1fb230f75dbd4725be8bbcb7e449aa98b9
Irozuku <emiliodu@gmail.com>
2025-08-07 17:07:58 -0400

    refactor: update Paper component styling in DataloaderConfiguration and Upload components

--- DashAI/front/src/components/datasets/Upload.jsx
+++ DashAI/front/src/components/datasets/Upload.jsx
@@ -124,7 +124,7 @@ function Upload({ onFileUpload }) {
   };
 
   return (
-    <Paper variant="outlined" sx={{ p: 4, height: "100%" }} square>
+    <Paper sx={{ p: 4, height: "100%" }} borderRadius={2}>
       <Grid container direction="column" rowSpacing={3}>
         {/* state text */}
         <Grid item sx={{ textAlign: "center" }}>
