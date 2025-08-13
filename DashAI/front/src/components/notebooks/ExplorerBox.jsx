b5f3fe432fa09d6cd361f547c9522cbebe889a8d
Irozuku <emiliodu@gmail.com>
2025-08-13 18:04:38 -0400

    feat: fetch and display explorer component name in ExplorerBox

--- DashAI/front/src/components/notebooks/ExplorerBox.jsx
+++ DashAI/front/src/components/notebooks/ExplorerBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   CardContent,
@@ -11,8 +11,25 @@ import {
 import { Analytics, Info } from "@mui/icons-material";
 import Results from "./ExplorerDetailTabs/Results";
 import { getExplorerStatus } from "../../utils/explorerStatus";
+import { getComponentById } from "../../api/component";
 
 export default function ExplorerBox({ explorer, handleExplorerDetailsClick }) {
+  const [explorerComponent, setexplorerComponent] = useState({});
+
+  useEffect(() => {
+    const fetchExplorerComponent = async () => {
+      const data = await getComponentById(explorer.exploration_type);
+      setexplorerComponent(data);
+    };
+    try {
+      fetchExplorerComponent();
+    } catch (error) {
+      console.error("Error fetching explorer component:", error);
+    }
+  }, []);
+
+  console.log(explorerComponent);
+
   return (
     <Card key={explorer.id} sx={{ bgcolor: "#212121", borderRadius: 2 }}>
       <CardContent>
@@ -26,7 +43,7 @@ export default function ExplorerBox({ explorer, handleExplorerDetailsClick }) {
         >
           <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
             <Analytics sx={{ color: "#00BEBB", fontSize: 20 }} />
-            <Typography variant="h6">{explorer.name}</Typography>
+            <Typography variant="h6">{explorerComponent.name}</Typography>
           </Box>
           <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
             <Chip
