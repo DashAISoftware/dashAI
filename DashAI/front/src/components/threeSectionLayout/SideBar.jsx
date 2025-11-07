b5f298f867f52a3e47c30cc6e44992271f3bfcdb
Irozuku <emiliodu@gmail.com>
2025-11-07 12:17:40 -0300

    refactor: adjust border styles and remove borderRadius from CenterBox and SideBar components

--- DashAI/front/src/components/threeSectionLayout/SideBar.jsx
+++ DashAI/front/src/components/threeSectionLayout/SideBar.jsx
@@ -1,18 +1,20 @@
 import { Box } from "@mui/material";
 
-export default function LeftBar({ children }) {
+export default function SideBar({ children }) {
   return (
     <Box
       width="100%"
       height="100%"
-      borderRadius={2}
+      // borderRadius={2}
       display="flex"
       flexDirection="column"
       justifyContent="space-between"
       sx={{
         bgcolor: "background.box",
         color: "white",
-        borderRight: "1px solid #252836",
+        borderTop: "0.1px solid",
+        borderBottom: "0.1px solid",
+        borderColor: "grey.600",
       }}
     >
       {children}
