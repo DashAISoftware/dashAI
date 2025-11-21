73eccff81778dbef4dc3262de4ec56cc887955ab
Irozuku <emiliodu@gmail.com>
2025-11-21 16:10:26 -0300

    refactor: comment out RunnerDialog in ExperimentsTable and adjust Results component usage

--- DashAI/front/src/components/experiments/ExperimentsTable.jsx
+++ DashAI/front/src/components/experiments/ExperimentsTable.jsx
@@ -133,14 +133,18 @@ function ExperimentsTable({
         type: "actions",
         minWidth: 180,
         getActions: (params) => [
-          <RunnerDialog
-            key="runner-dialog"
+          // <RunnerDialog
+          //   key="runner-dialog"
+          //   experiment={params.row}
+          //   expRunning={expRunning}
+          //   setExpRunning={setExpRunning}
+          //   deleteExperiment={() => handleDeleteExperiment(params.id)}
+          // />,
+          <Results
+            key="runs-dialog"
             experiment={params.row}
-            expRunning={expRunning}
-            setExpRunning={setExpRunning}
-            deleteExperiment={() => handleDeleteExperiment(params.id)}
+            handleDeleteExperiment={handleDeleteExperiment}
           />,
-          <Results key="runs-dialog" experiment={params.row} />,
           <DeleteItemModal
             key="delete-button"
             deleteFromTable={() => handleDeleteExperiment(params.id)}
