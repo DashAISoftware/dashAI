878e02e741cb77cd6b1f6bfca1e69f7725e7e50c
Irozuku <emiliodu@gmail.com>
2025-11-25 01:01:49 -0300

    feat: add optimizer check to reset current tab in ResultsDetails component

--- DashAI/front/src/pages/results/components/ResultsDetails.jsx
+++ DashAI/front/src/pages/results/components/ResultsDetails.jsx
@@ -3,16 +3,24 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { getRunById as getRunByIdRequest } from "../../../api/run";
 import ResultsDetailsLayout from "./ResultsDetailsLayout";
+import { checkIfHaveOptimazers } from "../../../utils/schema";
 
 /**
  * Component that renders multiple tabs to visualize the results of a specific run.
  */
 function ResultsDetails({ run, onClose, handleRun }) {
   const [currentTab, setCurrentTab] = useState(0);
+  const optimizers = checkIfHaveOptimazers(run.parameters);
 
   const handleTabChange = (event, newValue) => {
     setCurrentTab(newValue);
   };
+
+  useEffect(() => {
+    if (!optimizers) {
+      setCurrentTab(0);
+    }
+  }, [run.id]);
 
   return (
     <ResultsDetailsLayout
