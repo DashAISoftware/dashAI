3257fb78a189d076d32a6afa73746c89058dfe11
Irozuku <emiliodu@gmail.com>
2025-12-10 17:01:54 -0300

    refactor: remove unused prediction-related API functions and standardize naming conventions

--- DashAI/front/src/api/predict.ts
+++ DashAI/front/src/api/predict.ts
@@ -4,50 +4,9 @@ import type { IDataset } from "../types/dataset";
 import { IParamsFilter } from "../types/predict";
 const predictEndpoint = "/v1/predict";
 
-export const get_prediction_table = async (): Promise<object> => {
-  const response = await api.get(`${predictEndpoint}/prediction_table/`);
-  return response.data;
-};
-
-export const get_model_table = async (): Promise<object> => {
-  const response = await api.get(`${predictEndpoint}/model_table/`);
-  return response.data;
-};
-
-export const get_metadata_prediction_json = async (): Promise<object> => {
-  const response = await api.get(`${predictEndpoint}/metadata_json/`);
-  return response.data;
-};
-
-export const delete_prediction = async (
-  predict_name: string,
-): Promise<object> => {
-  const response = await api.delete(`${predictEndpoint}/${predict_name}`);
-  return response.data;
-};
-
-export const rename_prediction = async (
-  predict_name: string,
-  new_name: string,
-): Promise<object> => {
-  const response = await api.patch(`${predictEndpoint}/${predict_name}/`, {
-    new_name,
-  });
-  return response.data;
-};
-
-export const filter_datasets = async (requestData: IParamsFilter) => {
+export const filterDatasets = async (requestData: IParamsFilter) => {
   const response = await api.get(`${predictEndpoint}/filter_datasets`, {
     params: requestData,
-  });
-  return response.data;
-};
-
-export const get_predict_summary = async (predictionId: string) => {
-  const response = await api.get(`${predictEndpoint}/predict_summary`, {
-    params: {
-      pred_name: predictionId,
-    },
   });
   return response.data;
 };
@@ -92,4 +51,10 @@ export const getPredictionSummary = async (
     },
   });
   return response.data;
+};
+
+export const deletePrediction = async (
+  prediction_id: string,
+): Promise<void> => {
+  await api.delete(`${predictEndpoint}/${prediction_id}`);
 };
