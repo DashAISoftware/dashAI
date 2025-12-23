eb0c39052d422621f481779e1c8cd11efcc9d503
Irozuku <emiliodu@gmail.com>
2025-12-23 10:29:48 -0300

    feat: add Metric model to store metric information related to runs

--- DashAI/back/dependencies/database/models.py
+++ DashAI/back/dependencies/database/models.py
@@ -7,6 +7,7 @@ from sqlalchemy import JSON, Boolean, DateTime, Enum, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from DashAI.back.core.enums.metrics import LevelEnum, SplitEnum
 from DashAI.back.core.enums.plugin_tags import PluginTag
 from DashAI.back.core.enums.status import (
     ConverterListStatus,
@@ -134,10 +135,6 @@ class Run(Base):
     plot_importance_path: Mapped[str] = mapped_column(String, nullable=True)
     # goal metrics
     goal_metric: Mapped[str] = mapped_column(String)
-    # metrics
-    train_metrics: Mapped[JSON] = mapped_column(JSON, nullable=True)
-    test_metrics: Mapped[JSON] = mapped_column(JSON, nullable=True)
-    validation_metrics: Mapped[JSON] = mapped_column(JSON, nullable=True)
     # artifacts
     artifacts: Mapped[str] = mapped_column(JSON, nullable=True)
     # metadata
@@ -221,6 +218,18 @@ class Prediction(Base):
     def set_status_as_error(self) -> None:
         """Update the status of the prediction to error."""
         self.status = PredictionStatus.ERROR
+
+
+class Metric(Base):
+    __tablename__ = "metric"
+    """
+    Table to store all the information related to a metric
+    """
+    id: Mapped[int] = mapped_column(primary_key=True)
+    run_id: Mapped[int] = mapped_column(ForeignKey("run.id", ondelete="CASCADE"))
+    split: Mapped[SplitEnum] = mapped_column(Enum(SplitEnum), nullable=False)
+    level: Mapped[LevelEnum] = mapped_column(Enum(LevelEnum), nullable=False)
+    results: Mapped[Dict[str, list[float]]] = mapped_column(JSON, nullable=False)
 
 
 class Plugin(Base):
