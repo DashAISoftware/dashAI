5a291580d46f9d3d1e5882e8dcefb26f682e690a
Irozuku <emiliodu@gmail.com>
2025-12-26 11:29:14 -0300

    feat: add new classification metrics including Cohen Kappa, Hamming Distance, Log Loss, and RoC AUC

--- DashAI/back/metrics/__init__.py
+++ DashAI/back/metrics/__init__.py
@@ -1,9 +1,13 @@
 # flake8: noqa
 from DashAI.back.metrics.base_metric import BaseMetric
 from DashAI.back.metrics.classification.accuracy import Accuracy
+from DashAI.back.metrics.classification.cohen_kappa import CohenKappa
 from DashAI.back.metrics.classification.f1 import F1
+from DashAI.back.metrics.classification.hamming_distance import HammingDistance
+from DashAI.back.metrics.classification.log_loss import LogLoss
 from DashAI.back.metrics.classification.precision import Precision
 from DashAI.back.metrics.classification.recall import Recall
+from DashAI.back.metrics.classification.roc_auc import ROCAUC
 from DashAI.back.metrics.regression.mae import MAE
 from DashAI.back.metrics.regression.rmse import RMSE
 from DashAI.back.metrics.translation.bleu import Bleu
