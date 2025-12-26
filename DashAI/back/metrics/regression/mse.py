4da1d9b491011bcc7b15da10ea198a219aded09e
Irozuku <emiliodu@gmail.com>
2025-12-26 12:23:33 -0300

    feat: add new regression metrics including MSE, R2, Explained Variance, and Median Absolute Error

---
+++ DashAI/back/metrics/regression/mse.py
@@ -0,0 +1,38 @@
+"""DashAI MSE regression metric implementation."""
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
+from DashAI.back.metrics.regression_metric import RegressionMetric, prepare_to_metric
+
+
+class MSE(RegressionMetric):
+    """Mean Squared Error metric for regression tasks."""
+
+    DESCRIPTION: str = (
+        "Mean Squared Error (MSE) measures the average of the squared differences "
+        "between predicted values and actual values in a regression model. "
+        "It provides an indication of the model's prediction accuracy, with lower values "
+        "indicating better fit."
+    )
+
+    @staticmethod
+    def score(true_values: DashAIDataset, predicted_values: np.ndarray) -> float:
+        """Calculate the MSE between true values and predicted values.
+
+        Parameters
+        ----------
+        true_values : DashAIDataset
+            A DashAI dataset with true values.
+        predicted_values : np.ndarray
+            A one-dimensional array with the predicted values
+            for each instance.
+
+        Returns
+        -------
+        float
+            MSE score between true values and predicted values
+        """
+        true_values, pred_values = prepare_to_metric(true_values, predicted_values)
+        return mean_squared_error(true_values, pred_values)
