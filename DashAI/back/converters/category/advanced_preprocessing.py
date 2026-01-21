9f1f2c731ddda61c1043f5a5f8e887b648c4b01c
Irozuku <emiliodu@gmail.com>
2026-01-21 16:45:59 -0300

    feat: update CATEGORY in AdvancedPreprocessingConverter to use multilingual support

--- DashAI/back/converters/category/advanced_preprocessing.py
+++ DashAI/back/converters/category/advanced_preprocessing.py
@@ -1,8 +1,11 @@
 from beartype.typing import Final
 
 from DashAI.back.converters.base_converter import BaseConverter
+from DashAI.back.core.utils import MultilingualString
 
 
 class AdvancedPreprocessingConverter(BaseConverter):
-    CATEGORY: Final[str] = "Advanced Preprocessing"
+    CATEGORY: Final[str] = MultilingualString(
+        en="Advanced Preprocessing", es="Preprocesamiento Avanzado"
+    )
     COLOR: Final[str] = "rgb(70, 130, 180)"
