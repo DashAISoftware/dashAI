9aa738da6a6c72c9ac4debedda4d3e93eb600942
Irozuku <emiliodu@gmail.com>
2026-01-22 11:49:38 -0300

    feat: add multilingual support and icons to all converters

--- DashAI/back/converters/category/scaling_and_normalization.py
+++ DashAI/back/converters/category/scaling_and_normalization.py
@@ -1,8 +1,13 @@
 from beartype.typing import Final
 
 from DashAI.back.converters.base_converter import BaseConverter
+from DashAI.back.core.utils import MultilingualString
+from DashAI.back.static.icons import Icon
 
 
 class ScalingAndNormalizationConverter(BaseConverter):
-    CATEGORY: Final[str] = "Scaling and Normalization"
+    CATEGORY = MultilingualString(
+        en="Scaling and Normalization", es="Escalado y Normalización"
+    )
+    ICON: Final[str] = Icon.TrendingUp.value
     COLOR: Final[str] = "rgb(255, 165, 0)"
