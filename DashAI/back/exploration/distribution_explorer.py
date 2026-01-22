6e5baacfb02bc60e61161a6bb8a3596e3240072e
Irozuku <emiliodu@gmail.com>
2026-01-22 11:52:33 -0300

    feat: add icons to all explorers

--- DashAI/back/exploration/distribution_explorer.py
+++ DashAI/back/exploration/distribution_explorer.py
@@ -2,10 +2,12 @@ from beartype.typing import Final
 
 from DashAI.back.core.utils import MultilingualString
 from DashAI.back.exploration.base_explorer import BaseExplorer
+from DashAI.back.static.icons import Icon
 
 
 class DistributionExplorer(BaseExplorer):
     CATEGORY: Final[str] = MultilingualString(
         en="Distribution Analysis", es="Análisis de Distribución"
     )
+    ICON: Final[str] = Icon.BarChart.value
     COLOR: Final[str] = "rgb(155, 89, 182)"
--- DashAI/back/exploration/relationship_explorer.py
+++ DashAI/back/exploration/relationship_explorer.py
@@ -2,10 +2,12 @@ from beartype.typing import Final
 
 from DashAI.back.core.utils import MultilingualString
 from DashAI.back.exploration.base_explorer import BaseExplorer
+from DashAI.back.static.icons import Icon
 
 
 class RelationshipExplorer(BaseExplorer):
     CATEGORY: Final[str] = MultilingualString(
         en="Relationship Analysis", es="Análisis de Relaciones"
     )
+    ICON: Final[str] = Icon.ScatterPlot.value
     COLOR: Final[str] = "rgb(46, 204, 113)"
