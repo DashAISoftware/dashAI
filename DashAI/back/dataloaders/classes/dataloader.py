f52b4225762ed195af7c3e74e8d68be36ed39de9
Irozuku <emiliodu@gmail.com>
2026-02-05 15:23:46 -0300

    refactor: move pandas and datasets imports inside methods for lazy loading in BaseDataLoader

--- DashAI/back/dataloaders/classes/dataloader.py
+++ DashAI/back/dataloaders/classes/dataloader.py
@@ -1,13 +1,8 @@
 """DashAI base class for dataloaders."""
 
 import logging
-import os
-import zipfile
 from abc import abstractmethod
 from typing import Any, Dict, Final
-
-import pandas as pd
-from datasets.download.download_manager import DownloadManager
 
 from DashAI.back.config_object import ConfigObject
 from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
@@ -54,7 +49,7 @@ class BaseDataLoader(ConfigObject):
         filepath_or_buffer: str,
         params: Dict[str, Any],
         n_rows: int = 10,
-    ) -> pd.DataFrame:
+    ) -> any:
         """
         Load a preview of the dataset using streaming.
 
@@ -72,8 +67,8 @@ class BaseDataLoader(ConfigObject):
 
         Returns
         -------
-        pd.DataFrame
-            A DataFrame with the preview data.
+        DataFrame
+            A pandas DataFrame with the preview data.
         """
         raise NotImplementedError(
             "load_preview must be implemented by specific dataloader"
@@ -93,6 +88,8 @@ class BaseDataLoader(ConfigObject):
             type_path (str): Type of the path.
 
         """
+        from datasets.download.download_manager import DownloadManager
+
         if file_path.startswith("http"):
             file_path = DownloadManager.download_and_extract(file_path, temp_path)
             return (file_path, "dir")
@@ -116,6 +113,9 @@ class BaseDataLoader(ConfigObject):
         -------
             str: Path of the files extracted.
         """
+        import os
+        import zipfile
+
         files_path = os.path.join(temp_path, "files")
         os.makedirs(files_path, exist_ok=True)
         with zipfile.ZipFile(file_path, "r") as zip_ref:
