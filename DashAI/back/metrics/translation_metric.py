012525213ffef58e0e0dd7f95bb5da4bc12aa1c5
Irozuku <emiliodu@gmail.com>
2026-03-06 09:27:32 -0300

    refactor: move imports inside TYPE_CHECKING block for improved performance in regression_metric.py and translation_metric.py

--- DashAI/back/metrics/translation_metric.py
+++ DashAI/back/metrics/translation_metric.py
@@ -1,7 +1,11 @@
-import numpy as np
+from typing import TYPE_CHECKING
 
-from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
 from DashAI.back.metrics.base_metric import BaseMetric
+
+if TYPE_CHECKING:
+    from numpy import ndarray
+
+    from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
 
 
 class TranslationMetric(BaseMetric):
@@ -11,9 +15,9 @@ class TranslationMetric(BaseMetric):
 
 
 def prepare_to_metric(
-    y: DashAIDataset,
-    y_pred: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]:
+    y: "DashAIDataset",
+    y_pred: "ndarray",
+) -> tuple["ndarray", "ndarray"]:
     """Prepare data for metric calculation.
 
     Parameters
