186fa823041337c36c47df042354b7951acc3286
Irozuku <emiliodu@gmail.com>
2026-03-06 11:15:34 -0300

    refactor: defer imports and improve type hints in DatasetJob class for better performance

--- DashAI/back/job/dataset_job.py
+++ DashAI/back/job/dataset_job.py
@@ -1,23 +1,16 @@
-import json
 import logging
-import os
-import shutil
-import uuid
-from pathlib import Path
+from typing import TYPE_CHECKING
 
 from kink import di, inject
 from sqlalchemy import exc
-from sqlalchemy.orm import sessionmaker
 
 from DashAI.back.api.api_v1.schemas.datasets_params import DatasetParams
 from DashAI.back.api.utils import parse_params
-from DashAI.back.dataloaders.classes.dashai_dataset import (
-    load_dataset,
-    save_dataset,
-    transform_dataset_with_schema,
-)
 from DashAI.back.dependencies.database.models import Dataset, Notebook
 from DashAI.back.job.base_job import BaseJob, JobError
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import sessionmaker
 
 log = logging.getLogger(__name__)
 
@@ -41,7 +34,7 @@ class DatasetJob(BaseJob):
 
     @inject
     def set_status_as_delivered(
-        self, session_factory: sessionmaker = lambda di: di["session_factory"]
+        self, session_factory: "sessionmaker" = lambda di: di["session_factory"]
     ) -> None:
         """Set the status of the dataset as delivered."""
         dataset_id: int = self.kwargs["dataset_id"]
@@ -62,7 +55,7 @@ class DatasetJob(BaseJob):
 
     @inject
     def set_status_as_error(
-        self, session_factory: sessionmaker = lambda di: di["session_factory"]
+        self, session_factory: "sessionmaker" = lambda di: di["session_factory"]
     ) -> None:
         """Set the job status as error."""
         dataset_id: int = self.kwargs["dataset_id"]
@@ -97,7 +90,17 @@ class DatasetJob(BaseJob):
         self,
     ) -> None:
         import gc
+        import json
+        import os
+        import shutil
+        import uuid
+        from pathlib import Path
 
+        from DashAI.back.dataloaders.classes.dashai_dataset import (
+            load_dataset,
+            save_dataset,
+            transform_dataset_with_schema,
+        )
         from DashAI.back.types.inf.type_inference import infer_types
 
         component_registry = di["component_registry"]
