c58a8a615f5915a6a0aea6c33cf8a5bc4c532530
Irozuku <emiliodu@gmail.com>
2026-03-20 13:07:54 -0300

    fix: add TODO to flip onBrokenLinks to throw in Task 10

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   organizationName: 'DashAISoftware',
   projectName: 'DashAI',
 
+  // TODO(Task 10): change to 'throw' once all sidebar doc IDs have corresponding files
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
