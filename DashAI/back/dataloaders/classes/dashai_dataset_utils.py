dd928916b1771369d01c24ea54b11462fc878a09
Cristian Tamblay <cristian.tamblay@ing.uchile.cl>
2026-04-01 12:20:45 -0300

    Minor fixes

--- DashAI/back/dataloaders/classes/dashai_dataset_utils.py
+++ DashAI/back/dataloaders/classes/dashai_dataset_utils.py
@@ -244,6 +244,8 @@ def apply_categorical_one_hot_encoder(
     import pandas as pd
     import pyarrow as pa
 
+    from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
+
     if encoder is None or not categorical_cols:
         return dataset
 
@@ -281,12 +283,3 @@ def apply_categorical_one_hot_encoder(
     )
 
     return new_dataset
-
-
-# Time Transformations
-
-# Date Transformations
-
-
-# Image Transformations
-# Image Transformations
