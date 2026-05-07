232993747031f858e59208e7b8a550dc16c6df5d
Irozuku <emiliodu@gmail.com>
2026-05-07 17:53:56 -0400

    fix: correct indentation in ColumnSelector component with pre-commit

--- DashAI/front/src/components/notebooks/ColumnSelector.jsx
+++ DashAI/front/src/components/notebooks/ColumnSelector.jsx
@@ -338,8 +338,8 @@ function ColumnSelector({
               context: inputCardinality.exact
                 ? "exact"
                 : inputCardinality.max
-                ? "range"
-                : "min",
+                  ? "range"
+                  : "min",
             })}
           </Typography>
         )}
