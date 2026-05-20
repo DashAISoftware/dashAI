34c12eb0db6266ddba371f912a30b406d5513262
Irozuku <emiliodu@gmail.com>
2026-05-20 13:32:17 -0400

    feat: add ModuleThemeWrapper with contrasting hover/light colors per module

--- DashAI/front/src/App.jsx
+++ DashAI/front/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TourRegistryProvider } from "./contexts/TourRegistryContext";
+import ModuleThemeWrapper from "./components/ModuleThemeWrapper";
 
 import "./App.css";
 import DatasetsPage from "./pages/datasets/Datasets";
@@ -21,48 +22,56 @@ function App() {
       <BrowserRouter
         future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
       >
-        <ResponsiveAppBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/app" element={<Home />} />
-          <Route path="/app/data/" element={<DatasetsPage />} />
-          <Route path="/app/data/datasets/new" element={<DatasetsPage />} />
-          <Route
-            path="/app/data/datasets/new/:dataloaderName"
-            element={<DatasetsPage />}
-          />
-          <Route path="/app/data/datasets/:id" element={<DatasetsPage />} />
-          <Route path="/app/data/notebooks/new" element={<DatasetsPage />} />
-          <Route path="/app/data/notebooks/:id" element={<DatasetsPage />} />
-          <Route path="/app/models" element={<ModelsPage />} />
-          <Route path="/app/models/datasets/:id" element={<ModelsPage />} />
-          <Route path="/app/models/sessions/:id" element={<ModelsPage />} />
-          <Route
-            path="/app/models/sessions/new/:taskName"
-            element={<ModelsPage />}
-          />
-          <Route path="/app/generative" element={<Generative />} />
-          <Route path="/app/generative/sessions/new" element={<Generative />} />
-          <Route
-            path="/app/generative/sessions/new/:modelName"
-            element={<Generative />}
-          />
-          <Route path="/app/generative/sessions/:id" element={<Generative />} />
-          <Route path="/app/pipelines" element={<PipelinesPage />} />
-          <Route path="/app/pipelines/new" element={<NewPipelineWrapper />} />
-          <Route
-            path="/app/pipelines/:pipelineId"
-            element={<NewPipelineWrapper />}
-          />
-          <Route path="/app/plugins">
-            <Route index element={<PluginsPage />} />
-            <Route path=":category">
+        <ModuleThemeWrapper>
+          <ResponsiveAppBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/app" element={<Home />} />
+            <Route path="/app/data/" element={<DatasetsPage />} />
+            <Route path="/app/data/datasets/new" element={<DatasetsPage />} />
+            <Route
+              path="/app/data/datasets/new/:dataloaderName"
+              element={<DatasetsPage />}
+            />
+            <Route path="/app/data/datasets/:id" element={<DatasetsPage />} />
+            <Route path="/app/data/notebooks/new" element={<DatasetsPage />} />
+            <Route path="/app/data/notebooks/:id" element={<DatasetsPage />} />
+            <Route path="/app/models" element={<ModelsPage />} />
+            <Route path="/app/models/datasets/:id" element={<ModelsPage />} />
+            <Route path="/app/models/sessions/:id" element={<ModelsPage />} />
+            <Route
+              path="/app/models/sessions/new/:taskName"
+              element={<ModelsPage />}
+            />
+            <Route path="/app/generative" element={<Generative />} />
+            <Route
+              path="/app/generative/sessions/new"
+              element={<Generative />}
+            />
+            <Route
+              path="/app/generative/sessions/new/:modelName"
+              element={<Generative />}
+            />
+            <Route
+              path="/app/generative/sessions/:id"
+              element={<Generative />}
+            />
+            <Route path="/app/pipelines" element={<PipelinesPage />} />
+            <Route path="/app/pipelines/new" element={<NewPipelineWrapper />} />
+            <Route
+              path="/app/pipelines/:pipelineId"
+              element={<NewPipelineWrapper />}
+            />
+            <Route path="/app/plugins">
               <Route index element={<PluginsPage />} />
-              <Route path="details/:id" element={<PluginsDetails />} />
+              <Route path=":category">
+                <Route index element={<PluginsPage />} />
+                <Route path="details/:id" element={<PluginsDetails />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-        <JobQueueWidget />
+          </Routes>
+          <JobQueueWidget />
+        </ModuleThemeWrapper>
       </BrowserRouter>
     </TourRegistryProvider>
   );
