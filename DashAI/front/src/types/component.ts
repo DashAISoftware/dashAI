bae83b3256d2755533bc3859e7f1f3c263c5a4cf
Irozuku <emiliodu@gmail.com>
2026-06-15 12:07:00 -0400

    feat: add credentials api client and types

--- DashAI/front/src/types/component.ts
+++ DashAI/front/src/types/component.ts
@@ -10,4 +10,7 @@ export interface IComponent {
   description: string;
   display_name?: string;
   color?: string;
+  required_credentials?: string[];
+  optional_credentials?: string[];
+  credentials_satisfied?: boolean;
 }
---
+++ DashAI/front/src/types/credential.ts
@@ -0,0 +1,5 @@
+export interface ICredential {
+  name: string;
+  display_name: string;
+  is_authenticated: boolean;
+}
