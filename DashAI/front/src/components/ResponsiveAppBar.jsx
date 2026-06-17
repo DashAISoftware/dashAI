c15d82903cc6a6ea64ee6a50465e076569f7c06c
Creylay <careyesla@gmail.com>
2026-06-17 14:12:20 -0400

    fix: remove hub page from navigation in ResponsiveAppBar

--- DashAI/front/src/components/ResponsiveAppBar.jsx
+++ DashAI/front/src/components/ResponsiveAppBar.jsx
@@ -29,7 +29,6 @@ function ResponsiveAppBar() {
 
   const pages = [
     { name: t("common:datasets"), to: "/app/data" },
-    { name: t("common:hub"), to: "/app/data/hub" },
     { name: t("common:models"), to: "/app/models" },
     { name: t("common:generative"), to: "/app/generative" },
     { name: t("common:plugins"), to: "/app/plugins/browse" },
